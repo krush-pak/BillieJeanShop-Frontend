8f370b0f73a9af4746a1da188971dfbd4e0a8ef9
Aleksei Chkmrv <chkmrv@gmail.com>
2019-06-21 17:39:48 +0200

    fix submit form

--- src/components/LoginForm/components/LoginForm.js
+++ src/components/LoginForm/components/LoginForm.js
@@ -7,40 +7,46 @@ class LoginForm extends React.Component {
         super(props)
 
         this.state = {
+            name: '',
             email: 'test@test.educ',
             pass: 'test1'
         }
     }
 
-    handleSubmit = (data) => {
-        console.log('go')
+    handleSubmit = (e) => {
+        e.preventDefault();
 
+        const url = 'http://localhost:9000/api/auth/login';
 
-        axios.post('http://localhost:9000/api/auth/login', JSON.stringify({
-            'email': 'test@test.educ',
-            'password': 'test11'
-        }))
-        .then(function (response) {
-            console.log('response', response);
-            console.log('response.data', response.data);
+        axios.post(url, {
+            "email": this.state.email,
+            "password": this.state.pass,
+        })
+        .then((response) => {
+            this.setState({ name: response.data.user.name})
             console.log('response.data.user', response.data.user);
         })
-        .catch(function (error) {
-            console.log(error);
+        .catch((error) => {
+            console.log('error',error);
         });
+
+        return false;
     }
 
     render () {
         return (
             <div>
-                <Form inline onSubmit={e => this.handleSubmit(e)}>
-                    {/*<FormControl type="email" name='email' placeholder="Email" className="mr-sm-2" />*/}
-                    {/*<FormControl type="password" name='password' placeholder="Password" className="mr-sm-2" />*/}
-                    <Button
-                        type='submit'
-                        variant="outline-success"
-                    >Login</Button>
-                </Form>
+                {this.state.name !== ''
+                    ? <h3>Hello <strong>{this.state.name}</strong></h3>
+                    : <Form inline onSubmit={e => this.handleSubmit(e)}>
+                        <FormControl type="email" name='email' placeholder="Email" className="mr-sm-2" />
+                        <FormControl type="password" name='password' placeholder="Password" className="mr-sm-2" />
+                        <Button
+                            type='submit'
+                            variant="outline-success"
+                        >Login</Button>
+                    </Form>
+                }
             </div>
         )
     }
