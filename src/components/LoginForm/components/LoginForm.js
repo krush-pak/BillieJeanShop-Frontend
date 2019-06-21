10e6e28576cc48bf056e0443624f2308ff3805a5
Aleksei Chkmrv <chkmrv@gmail.com>
2019-06-21 17:54:50 +0200

    add validation

--- src/components/LoginForm/components/LoginForm.js
+++ src/components/LoginForm/components/LoginForm.js
@@ -8,6 +8,7 @@ class LoginForm extends React.Component {
 
         this.state = {
             name: '',
+            validated: false,
             email: 'test@test.educ',
             pass: 'test1'
         }
@@ -15,32 +16,41 @@ class LoginForm extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
+        const form = e.currentTarget;
+        console.log('e.currentTarget', form)
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        } else {
 
-        const url = 'http://localhost:9000/api/auth/login';
+            const url = 'http://localhost:9000/api/auth/login';
 
-        axios.post(url, {
-            "email": this.state.email,
-            "password": this.state.pass,
-        })
-        .then((response) => {
-            this.setState({ name: response.data.user.name})
-            console.log('response.data.user', response.data.user);
-        })
-        .catch((error) => {
-            console.log('error',error);
-        });
-
+            axios.post(url, {
+                "email": form.email.value,
+                "password": form.password.value,
+            })
+            .then((response) => {
+                this.setState({ name: response.data.user.name})
+                console.log('response.data.user', response.data.user);
+            })
+            .catch((error) => {
+                console.log('error',error);
+            });
+        }
+        this.setState({ validated: true });
         return false;
     }
 
     render () {
+        const { validated } = this.state;
         return (
             <div>
                 {this.state.name !== ''
                     ? <h3>Hello <strong>{this.state.name}</strong></h3>
-                    : <Form inline onSubmit={e => this.handleSubmit(e)}>
-                        <FormControl type="email" name='email' placeholder="Email" className="mr-sm-2" />
-                        <FormControl type="password" name='password' placeholder="Password" className="mr-sm-2" />
+                    : <Form noValidate
+                            validated={validated} inline onSubmit={e => this.handleSubmit(e)}>
+                        <FormControl type="email" name='email' placeholder="Email" className="mr-sm-2" required />
+                        <FormControl type="password" name='password' placeholder="Password" className="mr-sm-2"  required />
                         <Button
                             type='submit'
                             variant="outline-success"
